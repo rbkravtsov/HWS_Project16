bd51d379326d171c94ba001a78c19ea5bcd3244a
rbkravtsov <rkravtsov@me.com>
2019-01-27 12:09:54 +0300

    Add files via upload

---
+++ HWS_Project16/HWS_Project16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS_Project16
+//
+//  Created by Roman Kravtsov on 25/01/2019.
+//  Copyright © 2019 Roman Kravtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
